4388f903fb4513e4bb1e7eea855d35142f793a73
Wooseop Kim <wooseop@protonmail.com>
2024-08-04 15:02:28 +0900

    Extract import guard logic

--- phpypy/__init__.py
+++ phpypy/__init__.py
@@ -4,26 +4,7 @@ A Python-side wrapper for swoole/phpy
 
 __version__ = "0.0.1"
 
-import sys
-import os
-import platform
-
-os_name = {
-    "Linux": "linux",
-}[platform.system()]
-os_arch = {
-    "x86_64": "amd64",
-}[platform.machine()]
-lib_path = os.path.join(os.path.dirname(__file__), "..", "lib", os_name, os_arch)
-
-if not os.getenv("LD_LIBRARY_PATH", None):
-    os.environ["LD_LIBRARY_PATH"] = f"{lib_path}:{os.getenv('LD_LIBRARY_PATH', '')}"
-    os.execv(sys.executable, [sys.executable, sys.argv[0]])
-
-
-sys.path.append(lib_path)
-
-
-import phpy  # noqa: E402
+from . import _import_guard as _import_guard
+import phpy
 
 __all__ = ["phpy"]
---
+++ phpypy/_import_guard.py
@@ -0,0 +1,21 @@
+import sys
+import os
+import platform
+
+os_name = {
+    "Linux": "linux",
+}[platform.system()]
+os_arch = {
+    "x86_64": "amd64",
+}[platform.machine()]
+lib_path = os.path.join(os.path.dirname(__file__), "..", "lib", os_name, os_arch)
+
+if not len([x for x in os.listdir(lib_path) if x != ".gitkeep"]):
+    raise Exception(f"libraries are not found under {lib_path}")
+
+if not os.getenv("LD_LIBRARY_PATH", None):
+    os.environ["LD_LIBRARY_PATH"] = f"{lib_path}:{os.getenv('LD_LIBRARY_PATH', '')}"
+    os.execv(sys.executable, [sys.executable, sys.argv[0]])
+
+
+sys.path.append(lib_path)
