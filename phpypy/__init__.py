a7b420bf85115f940cf9512c00c10de0ef28c2c3
Wooseop Kim <wooseop@protonmail.com>
2025-01-15 22:05:41 +0900

    Add CLI script definition

--- phpypy/__init__.py
+++ phpypy/__init__.py
@@ -7,4 +7,10 @@ __version__ = "0.0.1"
 import phpy
 from phpypy import _import_guard as _import_guard
 
+
+def cli():
+    import sys
+    for arg in sys.argv[1:]:
+        phpy.eval(arg)
+
 __all__ = ["phpy"]
