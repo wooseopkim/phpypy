aeff3e8cfdd6b8caaf556da6236b8c50708a0ebf
Wooseop Kim <wooseop@protonmail.com>
2025-01-17 01:03:56 +0900

    Fix import order to be correct and prevent Ruff from breaking it

--- phpypy/__init__.py
+++ phpypy/__init__.py
@@ -4,8 +4,8 @@ A Python-side wrapper for swoole/phpy
 
 __version__ = "0.0.1"
 
+from phpypy import _import_guard as _import_guard  # noqa: I001
 import phpy
-from phpypy import _import_guard as _import_guard
 
 
 def cli():
